61e6a7b7919cd87ff0dcb68a844280043de47bbd
marin <marin.glavas@outlook.com>
2023-02-01 13:18:29 +0100

    add administration page

---
+++ Payroll/Views/Home/Administration.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PayrollApp.Views.Home
+{
+    public class AdministrationModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
